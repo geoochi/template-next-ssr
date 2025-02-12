1a9c7d69577423fb1121db97116b804390790abd
Geoo Chi <geoooochi@gmail.com>
2025-02-13 00:03:27 +0800

    init

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <p className='flex flex-col h-screen items-center justify-center text-3xl'>
+      template - next@15 - postcss@8 - tailwindcss@4
+    </p>
+  )
+}
