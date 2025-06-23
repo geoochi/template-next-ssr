33541c99cd9289e540f04f2dd8a8ae419f9739bc
Geoo Chi <geoochi@geoochi.com>
2025-06-23 21:47:12 +0800

    update

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,21 @@
+'use client'
 import Link from 'next/link'
+import { useState } from 'react'
+import { Button } from '@/components/ui/button'
 
 const About: React.FC = () => {
+  const [randomNum, setRandomNum] = useState(0)
+  const fetchRandomNum = () => {
+    fetch('/api/get-random-num')
+      .then((res) => res.json())
+      .then((data) => setRandomNum(data.res))
+  }
   return (
     <>
       <p className='text-4xl'>About Page</p>
       <Link href='./'>← Home Page</Link>
+      <Button onClick={() => fetchRandomNum()}>reset randomNum</Button>
+      <p>randomNum: {randomNum}</p>
     </>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,12 @@
-'use client'
+import Home from './Home'
 
-import { useTheme } from 'next-themes'
-import { Button } from '@/components/ui/button'
-import { SunIcon, MoonIcon } from 'lucide-react'
-import Link from 'next/link'
-
-const Home: React.FC = () => {
-  const { theme, setTheme } = useTheme()
-
+const Page: React.FC = () => {
   return (
     <>
-      <p className='text-3xl'>template - next@15 - shadcn@canary - tailwindcss@4</p>
-      <Button onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')} variant='ghost' size='icon'>
-        {theme === 'light' ? <SunIcon /> : <MoonIcon />}
-      </Button>
-      <Link href='./about'>→ About Page</Link>
+      <Home />
+      <p>PRIVATE_KEY: {process.env.PRIVATE_KEY}</p>
     </>
   )
 }
 
-export default Home
+export default Page
